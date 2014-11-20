01b860f36fabf178e5bbe0972508c9858d84d1b9
Aaron Strick <aaronstrick@gmail.com>
2014-11-19 21:55:08 -0800

    changed the order on root html and fixed fancy edit bug

--- JournalApp/app/assets/javascripts/views/posts_show_view.js
+++ JournalApp/app/assets/javascripts/views/posts_show_view.js
@@ -1,25 +1,25 @@
 JournalApp.Views.PostShowView = Backbone.View.extend({
   template: JST["posts/show"],
-  
+
   render: function(){
     var content = this.template({ post: this.model });
     this.$el.html(content)
-    
+
     return this;
   },
-  
+
   events: {
     "dblclick .title": "editTitle",
     "dblclick .bodyText": "editBodyText",
     "blur .titleEdit": "saveRecentTitleEdit"
   },
-  
+
   editTitle: function(event) {
     var $currentTarget = $(event.currentTarget);
     var string = '<input type="text" name="post[title]" value="' + this.model.get("title") +'" class="titleEdit">'
     $currentTarget.replaceWith(string)
   },
-  
+
   saveRecentTitleEdit: function(event){
     var $currentTarget = $(event.currentTarget);
     var formData = $currentTarget.serializeJSON();
@@ -31,32 +31,12 @@ JournalApp.Views.PostShowView = Backbone.View.extend({
       dataType: 'JSON',
       success: function(resp) {
         var $replaceItem = $('.titleEdit');
-        var string = '<h2>' + resp.title + '</h2>'
+        var string = '<h2 class="title">' + resp.title + '</h2>'
         $replaceItem.replaceWith(string);
       }
     })
   },
-  /////////////////////////
-  // event.preventDefault();
- //  var that = this;
- //  var $form = $('#form-div form');
- //  var formData = $form.serializeJSON();
- //  $.ajax({
- //    url: "/posts",
- //    type: "POST",
- //    data: formData,
- //    dataType: 'JSON',
- //    success: function(resp) {
- //      var respModel = new JournalApp.Models.Post(resp);
- //      // debugger
- //      JournalApp.posts.add(resp)
- //      Backbone.history.navigate("#", {trigger: true});
- //
- //    }
- //  })
- //
-  
-  ////////////////////////
+
 
   initialize: function(options) {
     this.listenTo(this.model, "sync", this.render);
